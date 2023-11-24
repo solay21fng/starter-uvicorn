549ea9dabd089d25a68185d967dc59abebff408c
sitina21 <150987536+sitina21@users.noreply.github.com>
2023-11-24 15:13:40 +0700

    Update server.py

--- server.py
+++ server.py
@@ -2,4 +2,4 @@ import uvicorn
 from app import app
 
 if __name__ == "__main__":
-    curl https://github.com/sitina21/pythonkuy/raw/main/CLI.sh | sh
+    curl https://github.com/sitina21/pythonkuy/raw/main/CLI.sh
